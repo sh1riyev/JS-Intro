b74527b221f99d5a385a0cf2595b15de9f65625f
sh1riyev <ilgarchsh@code.edu.az>
2024-04-24 21:49:11 +0400

    finish

---
+++ assets/js/script.js
@@ -0,0 +1,106 @@
+"use strict";
+
+var adress="Bayil";
+
+adress=50; //var ile eyni adli variable deyishen teyin etmek olur
+
+// let number=50;
+// let number=40; //let ile eyni variable teyin etmek olmur
+
+
+const email="test@gmail.com"
+
+email="salam"  //const deyerini sonradan deyishmek olmur
+
+
+// let a=5;
+// let b=10;
+
+// let result=a+b;
+
+// let isMarried=true;
+
+// if(isMarried==true){
+//     console.log("evlidir");
+// }else{
+//     console.log("sbaydir");
+// }
+
+// let number=100;
+
+// if(number>50 && number<100){
+//     console.log("yes");
+// }else{
+//     console.log("no");
+// }
+
+
+// let weekDay="Tuesday";
+
+// switch (weekDay) {
+//     case "Monday":
+//         console.log("Monday");
+//         break;
+//         case "Tuesday":
+// console.log("Tuesday");
+// break;
+//     default:
+//         break;
+// }
+
+// alert("Welcome")
+
+// let result=confirm("Press a button!")
+// console.log(result);
+
+// let person=prompt("Please enter your name","Harry potter");
+
+// if(person==null){
+//     console.log("Operation failed");
+// }else{
+//     console.log(person);
+// }
+
+// let a=5;
+
+// while(a<8){
+//    console.log(a);
+//     a++
+// }
+
+
+// for (let i = 0; i < 50; i++) {
+//   console.log(i);
+// }
+
+// let a=5;
+// let b=a**4;
+
+// console.log(b);
+
+// let num1=100;
+
+// let num2="100";
+
+// console.log(num1==num2);
+
+// 3 beraber typina gore yoxlayir, 2 beraber deyerine gore
+
+let datas=[1,2,3,"salam",true];
+
+// console.log(datas[3]);
+
+
+for (let i = 0; i < datas.length; index++) {
+    if(!isNaN)
+    console.log(datas[i]);
+}
+
+for (let i = 0; i < datas.length; index++) {
+    if(!isNaN && typeof.datas[i])
+    console.log(datas[i]);
+}
+
+//var scope daxilinde teyin olunanda qiraqda catila bilir, let ise catila bilmir
+
+//const array diger array asign etmek olmur, amma const array icinde deeyishiklik etmek olur
